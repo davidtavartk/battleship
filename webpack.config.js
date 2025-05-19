8ffd167b6ab6136651281414b794a30946cdc430
davidtavartk <d.tavartkiladze5@gmail.com>
2025-05-19 16:12:14 +0400

    init: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    target: 'node',
+    entry: path.resolve(__dirname, 'index.ts'),
+    output: { 
+        filename: 'index.js' 
+    },
+    module: { 
+        rules: 
+        [
+            { 
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                exclude: ['/node_modules/'],
+            }
+        ]
+    },
+    resolve: { 
+        extensions: ['.js', '.ts']
+    },
+    externals: [
+        {
+            'utf-8-validate': 'commonjs utf-8-validate',
+            bufferutil: 'commonjs bufferutil',
+        },
+    ],
+}
